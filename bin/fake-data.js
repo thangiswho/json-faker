6c40737f410088f57d3078faccd60b1a00356122
thangiswho <thangiswho@gmail.com>
2021-04-09 00:20:29 +0900

    add bin file fake-data

---
+++ bin/fake-data.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const { SimpleFaker } = require("../dist");
+const fs = require("fs");
+const yargs = require("yargs");
+
+const fakeData = function (argv) {
+  const schemaFile = argv._[0];
+  if (!fs.existsSync(schemaFile)) {
+    console.error("Error: schema file [" + schemaFile + "] does not exist!");
+    return;
+  }
+  try {
+    schema = JSON.parse(fs.readFileSync(schemaFile));
+    if (!schema || typeof schema !== "object") {
+      console.error(
+        "Error: schema file [" + schemaFile + "] is not a valid json file"
+      );
+      return;
+    }
+
+    const faker = new SimpleFaker(argv.locale, argv.length);
+    const api = JSON.stringify(faker.fakeApi(schema), null, 2);
+
+    if (argv.output.length) {
+      fs.writeFileSync(argv.output, api);
+      console.log("Fake data was saved successfully to: " + argv.output);
+    } else {
+      console.log(api);
+    }
+  } catch (e) {
+    console.error("Error: Could not fake data for schema [" + schemaFile + "]");
+    console.error(e.message);
+    return;
+  }
+};
+
+(function () {
+  const argv = yargs
+    .usage("$0 [options] <schema json file>")
+    .options({
+      length: {
+        alias: "n",
+        description: "The length of each generated data set",
+        default: 10,
+      },
+      locale: {
+        alias: "l",
+        description: 'The locale (eg. "ja", "de")',
+        default: "en",
+      },
+      output: {
+        alias: "o",
+        description: "Write faked data to file",
+        default: "",
+      },
+    })
+    .require(1, "Missing <schema json file> argument").argv;
+
+  fakeData(argv);
+})();
